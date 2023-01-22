9f38e5c41a79f258f5cdb024ae9a47de26dd7ad6
Anu Khadka <anuragi.khadka@gmail.com>
2023-01-22 17:19:01 -0500

    name change

---
+++ public/js/profile.js
@@ -0,0 +1,23 @@
+
+const delButtonHandler = async (event) => {
+  if (event.target.hasAttribute('data-id')) {
+    const id = event.target.getAttribute('data-id');
+
+    const response = await fetch(`/api/books/${id}`, {
+      method: 'DELETE',
+    });
+
+    if (response.ok) {
+      document.location.replace('/profile');
+    } else {
+      alert('Failed to remove book from inventory');
+    }
+  }
+};
+
+
+
+document
+  .querySelector('.book-list')
+  .addEventListener('click', delButtonHandler);
+
