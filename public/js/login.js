2f31aecac7e8d2f3db08f559cf9299ba5f965923
Zeizil <ekphilbrook@gmail.com>
2023-01-25 17:18:19 -0500

    think that this has some stuff that's on the heroku page

--- public/js/login.js
+++ public/js/login.js
@@ -15,7 +15,7 @@ const loginFormHandler = async (event) => {
 
     if (response.ok) {
       // If successful, redirect the browser to the profile page
-      document.location.replace('/yourBooks');
+      document.location.replace('/mybooks');
     } else {
       alert(response.statusText);
     }
